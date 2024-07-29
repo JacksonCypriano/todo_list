c6d2fba4f47daab1863cad39c1c4f3293e604804
Jackson Cypriano Cobra <jackson.cobra@cargobr.com>
2024-07-29 12:25:14 -0300

    git commit -m "Ajustes na configuração para Railway"

--- wait-for-postgres.sh
+++ wait-for-postgres.sh
@@ -6,14 +6,12 @@ db_url="$DATABASE_URL"
 shift
 cmd="$@"
 
-# Extract components from DATABASE_URL
-PGPASSWORD=$(echo "$db_url" | sed -e 's,^.*://[^:]*:\([^@]*\)@.*, \1,')
-PGUSER=$(echo "$db_url" | sed -e 's,^.*://\([^:]*\):.*,\1,')
-PGHOST=$(echo "$db_url" | sed -e 's,^.*://[^@]*@\(.*\):.*,\1,')
-PGPORT=$(echo "$db_url" | sed -e 's,^.*:\([0-9]*\)/.*,\1,')
-PGDATABASE=$(echo "$db_url" | sed -e 's,^.*/\([^?]*\).*,\1,')
-
-until PGPASSWORD=$PGPASSWORD PGUSER=$PGUSER PGHOST=$PGHOST PGPORT=$PGPORT PGDATABASE=$PGDATABASE psql -h "$PGHOST" -U "$PGUSER" -c '\q'; do
+until PGPASSWORD=$(echo "$db_url" | sed -e 's,^.*://[^:]*:\([^@]*\)@.*, \1,') \
+      PGUSER=$(echo "$db_url" | sed -e 's,^.*://\([^:]*\):.*,\1,') \
+      PGHOST=$(echo "$db_url" | sed -e 's,^.*://[^@]*@\(.*\):.*,\1,') \
+      PGPORT=$(echo "$db_url" | sed -e 's,^.*:\([0-9]*\)/.*,\1,') \
+      PGDATABASE=$(echo "$db_url" | sed -e 's,^.*/\([^?]*\).*,\1,') \
+      psql -h "$PGHOST" -U "$PGUSER" -c '\q'; do
   >&2 echo "Postgres is unavailable - sleeping"
   sleep 1
 done
